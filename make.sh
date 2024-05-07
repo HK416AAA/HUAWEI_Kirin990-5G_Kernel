3de1f06edae3e675c763296cdca08cc38f643500
HK416AAA <136133480+HK416AAA@users.noreply.github.com>
2024-05-07 19:32:00 +0800

    Delete make.sh

--- make.sh
+++
@@ -1,9 +0,0 @@
-export ARCH=arm64
-
-export PATH=$PATH:/home/gcc4.9/bin
-
-export CROSS_COMPILE=aarch64-linux-android-
-
-make ARCH=arm64 O=out merge_kirin990_defconfig
-
-make ARCH=arm64 O=out -j16
